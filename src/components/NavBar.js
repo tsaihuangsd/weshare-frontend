e9347394c86845aa83b08f78c768fc602360429b
Tsai Huang <tsaihuangsd@gmail.com>
2021-11-08 14:45:48 -0800

    Replace Material-UI dependency with MUI.  Implement responsive NavBar styling with basic features.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,70 +2,175 @@ import React, {useState} from 'react'
 import {useHistory,} from 'react-router-dom'
 import './styles/NavBar.css'
 import {
-  AppBar,
-  Toolbar,
+  // makeStyles,
+  // Link,
   CssBaseline,
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
   Typography,
-  makeStyles,
-  Link,
-} from "@material-ui/core";
-// import List from '@material-ui/core/List';
-// import ListItem from '@material-ui/core/ListItem';
-// import ListItemText from '@material-ui/core/ListItemText';
+  Badge,
+  MenuItem,
+  Menu,
+} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+// import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+// import MailIcon from '@mui/icons-material/Mail';
+// import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
 
-const useStyles = makeStyles((theme) => ({
-  navlinks: {
-    marginLeft: theme.spacing(10),
-    display: "flex",
-  },
-  logo: {
-    // marginLeft: "0px",
-    color: "white",
-    flexGrow: "1",
-    cursor: "pointer",
-  },
-  link: {
-    textDecoration: "none",
-    color: "white",
-    fontSize: "20px",
-    marginLeft: theme.spacing(20),
-    "&:hover": {
-      color: "yellow",
-      cursor: "pointer",
-      // borderBottom: "1px solid white",
-    },
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   navlinks: {
+//     marginLeft: theme.spacing(10),
+//     display: "flex",
+//   },
+//   logo: {
+//     // marginLeft: "0px",
+//     color: "white",
+//     flexGrow: "1",
+//     cursor: "pointer",
+//   },
+//   link: {
+//     textDecoration: "none",
+//     color: "white",
+//     fontSize: "20px",
+//     marginLeft: theme.spacing(20),
+//     "&:hover": {
+//       color: "yellow",
+//       cursor: "pointer",
+//       // borderBottom: "1px solid white",
+//     },
+//   },
+// }));
 
-const NavBar = (props) => {
-  // const [anchorEl, setAnchorEl] = React.useState(null);
-  // const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-  // const isMenuOpen = Boolean(anchorEl);
-  // const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-  const pathname = window.location.href
+export default function NavBar(props) {
+  // const pathname = window.location.href
+  // const classes = useStyles()
   const frontendURL = process.env.REACT_APP_FRONTEND_URL
-  const classes = useStyles()
+
+  const [anchorEl, setAnchorEl] = useState(false);
+  const [mobileAnchorEl, setMobileAnchorEl] = useState(false);
+  
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileAnchorEl(false);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(false);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderUserMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={anchorEl}
+      onClose={handleMenuClose} >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileUserMenu = (
+    <Menu
+      anchorEl={mobileAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={mobileAnchorEl}
+      onClose={handleMobileMenuClose} >
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit" >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (<>
-    <AppBar position="static">
-      <CssBaseline />
-      <Toolbar>
-        <Link href="/" variant="h4" className={classes.logo}>
-          WeShare
-        </Link>
-        <div className={classes.navlinks}>
-          <Link href="/" className={classes.link}>
-            Home
-          </Link>
-          <Link href="/groups" className={classes.link}>
-            Groups
-          </Link> 
-        </div>
-      </Toolbar>
-    </AppBar>
-
-  </>)
+    <CssBaseline />
+    <Box sx={{ flexGrow: 1 }}>      
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }} >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }} >
+            WeShare
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit" >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit">
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileUserMenu}
+      {renderUserMenu}
+    </Box>
+  </>
+  )
 }
-export default NavBar
+
