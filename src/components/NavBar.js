a81a4c608d39931bdb3783e453e46b5d0bd600d4
Tsai Huang <tsaihuangsd@gmail.com>
2021-11-08 10:45:23 -0800

    Setup basic Navigation Bar with Material UI styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,46 +1,71 @@
 import React, {useState} from 'react'
-import {useHistory} from 'react-router-dom'
+import {useHistory,} from 'react-router-dom'
 import './styles/NavBar.css'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import TypoGraphy from '@material-ui/core/Typography'
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+  Link,
+} from "@material-ui/core";
+// import List from '@material-ui/core/List';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemText from '@material-ui/core/ListItemText';
+
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginLeft: theme.spacing(10),
+    display: "flex",
+  },
+  logo: {
+    // marginLeft: "0px",
+    color: "white",
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(20),
+    "&:hover": {
+      color: "yellow",
+      cursor: "pointer",
+      // borderBottom: "1px solid white",
+    },
+  },
+}));
 
 const NavBar = (props) => {
+  // const [anchorEl, setAnchorEl] = React.useState(null);
+  // const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  // const isMenuOpen = Boolean(anchorEl);
+  // const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
   const pathname = window.location.href
   const frontendURL = process.env.REACT_APP_FRONTEND_URL
+  const classes = useStyles()
 
   return (<>
-    <AppBar color="primary" position="static">
+    <AppBar position="static">
+      <CssBaseline />
       <Toolbar>
-        <TypoGraphy variant="title" color="inherit" >
+        <Link href="/" variant="h4" className={classes.logo}>
           WeShare
-        </TypoGraphy>
-        
-        <List component="nav">
-          <ListItem component="div">
-            <ListItemText inset>
-              <TypoGraphy color="inherit" variant="title">
-                Home
-              </TypoGraphy>
-            </ListItemText>
-            <ListItemText inset>
-              <TypoGraphy color="inherit" variant="title">
-                Groups
-              </TypoGraphy>
-            </ListItemText>
-            <ListItemText inset>
-              <TypoGraphy color="inherit" variant="title">
-                My Account
-              </TypoGraphy>
-            </ListItemText>
-          </ListItem >
-        </List>
+        </Link>
+        <div className={classes.navlinks}>
+          <Link href="/" className={classes.link}>
+            Home
+          </Link>
+          <Link href="/groups" className={classes.link}>
+            Groups
+          </Link> 
+        </div>
       </Toolbar>
     </AppBar>
-    
+
   </>)
 }
 export default NavBar
